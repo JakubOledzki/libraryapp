d68cd286852a1467c3b8faca55c87bbbfce6aac3
JakubOledzki <jakuboledzki21@gmail.com>
2020-05-12 18:11:39 +0200

    Create project

---
+++ src/main/java/com/joledzki/LibraryappApplication.java
@@ -0,0 +1,13 @@
+package com.joledzki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryappApplication.class, args);
+    }
+
+}
